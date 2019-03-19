e0ebc7406b1c87f6f81344f7299b4f33dfa6d204
Andrew <fedyk72@gmail.com>
2019-03-19 01:04:37 +0100

    Update overview.ts

--- src/overview.ts
+++ src/overview.ts
@@ -4,9 +4,11 @@ import { createDrag } from "./drag";
 import { createLine } from "./line";
 import { generalUpdatePattern } from "./general-update-pattern";
 import { setAttribute } from "./set-attribute";
+import { removeElement } from "./remove-element";
 
 const CONTROL_WIDTH = 4;
-const HANDLER_WIDTH = 6;
+const HANDLER_WIDTH = 16;
+const HANDLE_EXTRA_SPACE = 5;
 
 export function createOverview() {
   let height = 38;
@@ -14,9 +16,8 @@ export function createOverview() {
   let lines = [].map(() => createLine())
   let x = createScale([0, 1], [0, 1])
   let y = createScale([0, 1], [height, 0])
-  let selectedDomain: [number, number] = [0, 1]
-
-  const drag = createDrag();
+  let selection: [number, number] = [0, 1]
+  let changeSelection = function(_) {}
 
   function renderLines(target) {
     const allLines = generalUpdatePattern<Line>(target, ".line", data.lines);
@@ -33,11 +34,11 @@ export function createOverview() {
     })
 
     // remove not needed lines
-    allLines.exit(line => line.parentElement !== null && line.parentElement.removeChild(line))
+    allLines.exit(line => removeElement(line))
   }
 
   function renderSelectionEdges(target) {
-    const update = generalUpdatePattern<number>(target, ".overview-control", selectedDomain)
+    const update = generalUpdatePattern<number>(target, ".overview-control", selection)
 
     update.enter((d) => {
       const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect")
@@ -57,8 +58,8 @@ export function createOverview() {
 
   function renderSelectionTopBorder(target) {
     const update = generalUpdatePattern<[number, [number, number]]>(target, ".overview-control-border", [
-      [0, selectedDomain],
-      [height, selectedDomain]
+      [0, selection],
+      [height, selection]
     ])
 
     update.enter(() => {
@@ -79,7 +80,7 @@ export function createOverview() {
   }
 
   function renderOverlays(target) {
-    const overviewOverlays = generalUpdatePattern(target, ".overview-overlay", selectedDomain)
+    const overviewOverlays = generalUpdatePattern(target, ".overview-overlay", selection)
 
     overviewOverlays.enter(() => {
       const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
@@ -88,53 +89,129 @@ export function createOverview() {
       setAttribute(rect, "class", "overview-overlay")
       setAttribute(rect, "fill", "#F2F7FA")
       setAttribute(rect, "fill-opacity", "0.8")
+      setAttribute(rect, "y", "0")
+      setAttribute(rect, "height", height + "")
 
       return rect
     }).merge((rect, datum, index) => {
-      const rectX = index === 0 ? 0 : x(datum);
-      const rectY = 0;
-      const rectW = index === 0 ? (x(datum) - CONTROL_WIDTH / 2) : (x.range()[1] - x(datum) + CONTROL_WIDTH / 2);
+      const [r0, r1] = x.range() as [number, number];
+      const rectX = index === 0 ? r0 : x(datum);
+      const rectW = index === 0 ? (x(datum) - CONTROL_WIDTH / 2) : (r1 - x(datum) + CONTROL_WIDTH / 2);
 
       setAttribute(rect, "x", rectX + "")
-      setAttribute(rect, "y", rectY + "")
-      setAttribute(rect, "width", Math.max(rectW, 0) + "")
-      setAttribute(rect, "height", height + "")
+      setAttribute(rect, "width", rectW + "")
     })
   }
 
-  function renderSelectionSideHandlers(target) {
-    const update = generalUpdatePattern<number>(target, ".overview-handlers", selectedDomain)
+  function renderSelectionCenterHandler(target) {
+    const update = generalUpdatePattern<[number, number]>(target, ".overview-center-handler", [selection])
 
     update.enter((d, i) => {
       const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect")
-
-      setAttribute(rect, "width", HANDLER_WIDTH + "")
+      const drag = createDrag();
+      
+      rect.style.cursor = "grab";
       setAttribute(rect, "height", height + "")
-      setAttribute(rect, "class", "overview-handlers")
-      setAttribute(rect, "y", 0 + "")
+      setAttribute(rect, "fill-opacity", "0.4")
+      setAttribute(rect, "fill", "#DCEDC8")
+      setAttribute(rect, "class", "overview-center-handler")
+      setAttribute(rect, "y", 0 - HANDLE_EXTRA_SPACE + "")
 
-      drag(rect).container(() => target).on("start", (event) => {
+      drag(rect).container(function() {
+        return target
+      })
+      .on("start", function(event) {
+        const [d0, d1] = x.domain() as [number, number];
+        const [s0, s1] = selection;
+        const ds = s1 - s0;
+        const dx = event.x - x(s0);
+
+        function normalize(event): [number, number] {
+          const d3 = x.invert(event.x - dx);
+          const d4 = d3 + ds;
+
+          const r5 = Math.min(Math.max(d3, d0), d1 - ds);
+          const r6 = Math.min(Math.max(d4, d0 + ds), d1);
+
+          return [r5, r6]
+        }
+
         event.on("drag", (event) => {
-          const selection = selectedDomain.concat() as [number, number];
-
-          selection[i] = Math.min(
-            Math.max(Math.round(x.invert(event.x)), x.domain()[0]), x.domain()[1]
-          );
-          selectedDomain = selection;
-          renderSelectionSideHandlers(target)
-          renderSelectionTopBorder(target)
-          renderSelectionEdges(target)
+          selection = normalize(event)
+          changeSelection(selection);
+          renderSelectedArea(target);
         })
 
         event.on("end", (event) => {
-          console.log(event.x)
+          selection = normalize(event)
+          changeSelection(selection);
+          renderSelectedArea(target);
         })
       })
 
       return rect
     }).merge((rect, datum) => {
+      setAttribute(rect, "x", x(datum[0]) + (HANDLER_WIDTH / 2) + "")
+      setAttribute(rect, "width", x(datum[1]) - x(datum[0]) - 2 * (HANDLER_WIDTH / 2) + "")
+      setAttribute(rect, "height", height + 2 * HANDLE_EXTRA_SPACE + "")
+    })
+  }
+
+  function renderSelectionSideHandlers(target) {
+    const update = generalUpdatePattern<number>(target, ".overview-handlers", selection)
+
+    update.enter(function(d, i) {
+      const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect")
+      const drag = createDrag();
+      
+      rect.style.cursor = "ew-resize";
+      setAttribute(rect, "width", HANDLER_WIDTH + "")
+      setAttribute(rect, "height", height + 2 * HANDLE_EXTRA_SPACE + "")
+      setAttribute(rect, "fill-opacity", "0.6")
+      setAttribute(rect, "fill", "#ccc")
+      setAttribute(rect, "class", "overview-handlers")
+      setAttribute(rect, "y", 0 - HANDLE_EXTRA_SPACE + "")
+
+      console.log(`render i=${i}`)
+
+      drag(rect).container(() => target).on("start", (event) => {
+        const r0 = selection[Number(!i)];
+        const cr = selection[i];
+        const dx = event.x - x(cr);
+        const [d0, d1] = x.domain() as [number, number];
+
+        function normalize(event) {
+          const r2 = x.invert(event.x - dx);
+          const r3 = Math.min(Math.max(r2, d0), d1);
+
+          return [r0, r3].sort() as [number, number];
+        }
+
+        event.on("drag", function(event) {
+          selection = normalize(event)
+          changeSelection(selection);
+          renderSelectedArea(target);
+        })
+
+        event.on("end", function(event) {
+          selection = normalize(event)
+          changeSelection(selection);
+          renderSelectedArea(target);
+        })
+      })
+
+      return rect
+    }).merge(function(rect, datum) {
       setAttribute(rect, "x", x(datum) - (HANDLER_WIDTH / 2) + "")
     })
+  }
+
+  function renderSelectedArea(target) {
+    renderOverlays(target)
+    renderSelectionEdges(target)
+    renderSelectionTopBorder(target)
+    renderSelectionSideHandlers(target)
+    renderSelectionCenterHandler(target)
   }
 
   function render(target: SVGGElement) {
@@ -142,10 +219,11 @@ export function createOverview() {
     target.style.fill = "none";
     target.style["-webkit-tap-highlight-color"] = "rgba(0,0,0,0)";
     renderLines(target)
+    renderOverlays(target)
     renderSelectionEdges(target)
     renderSelectionTopBorder(target)
-    renderOverlays(target)
     renderSelectionSideHandlers(target)
+    renderSelectionCenterHandler(target)
   }
 
   render.xRange = function (_: [number, number]) {
@@ -159,6 +237,14 @@ export function createOverview() {
   render.height = function (_) {
     return height = _, render;
   }
+  
+  render.changeSelection = function (_) {
+    return changeSelection = _, render;
+  }
+  
+  render.selection = function (_) {
+    return selection = _, render;
+  }
 
   render.data = function (_: Chart) {
     data = _;
@@ -166,7 +252,6 @@ export function createOverview() {
       .x((d, i) => x(data.x[i]))
       .y(d => y(d))
     )
-    selectedDomain = [data.xDomain[0], data.xDomain[1]];
     x.domain(data.xDomain);
     y.domain(data.linesDomain);
     return render;
