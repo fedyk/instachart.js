528d55c2fddb118a7ca8420d97be8e6f61c828b9
Andrew <fedyk72@gmail.com>
2019-03-19 23:08:23 +0100

    Create class-list.ts

---
+++ src/class-list.ts
@@ -0,0 +1,28 @@
+export function addClass(el: HTMLElement, className: string) {
+  return el.className += ' ' + className, el
+}
+
+export function removeClass(el: Element, className: string) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+
+  return el
+}
+
+export function toggleClass(el: Element, className: string) {
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    el.className = classes.join(' ');
+  }
+}
