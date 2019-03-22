846a32a7c19eabf1d0a1f652c2ad2d735f9a9a73
Andrew <fedyk72@gmail.com>
2019-03-22 23:26:22 +0100

    Speed up toggle night mode

--- src/toggle-night-mode.ts
+++ src/toggle-night-mode.ts
@@ -1,11 +1,15 @@
-import { preventDefault } from "./prevent-default";
 import { toggleClass } from "./class-list";
 import { createRipple } from "./ripple";
+import { preventDefault } from "./prevent-default";
+import { noPropagation } from "./no-propagation";
 
 declare const event: UIEvent;
 
 export function toggleNightMode() {
   const button = event.currentTarget as HTMLButtonElement | null;
+
+  preventDefault();
+  noPropagation();
 
   if (button && !button["__ripple__"]) {
     button["__ripple__"] = createRipple(button);
