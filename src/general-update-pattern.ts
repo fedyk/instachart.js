7d67d2569d4a359c8d3afe6d6f488f9a858440a7
Andrew <fedyk72@gmail.com>
2019-03-24 22:24:45 +0100

    add bind by key for GUP

--- src/general-update-pattern.ts
+++ src/general-update-pattern.ts
@@ -1,11 +1,7 @@
-export function generalUpdatePattern<T>(parent: Element, selector: string, data: T[]) {
-  const elements = parent.querySelectorAll(selector);
-  const elementsLength = elements.length;
+function bindByIndex<T>(parent: Element, elements: NodeListOf<Element>, enterGroup: EnterNode<T>[], updateGroup: Element[], exitGroup: Element[], data: T[]) {
   const dataLength = data.length;
-  const enterGroup: EnterNode<T>[] = new Array(dataLength);
-  const enteredGroup: Element[] = new Array(dataLength);
-  const updateGroup: Element[] = new Array(dataLength);
-  const exitGroup: Element[] = new Array(elementsLength);
+  const elementsLength = elements.length;
+
   let i = 0;
 
   for (; i < dataLength; i++) {
@@ -26,6 +22,79 @@ export function generalUpdatePattern<T>(parent: Element, selector: string, data:
       exitGroup[i] = element;
     }
   }
+}
+
+const KEY_PREFIX = "$";
+
+function bindByKey<T>(parent: Element, elements: NodeListOf<Element>, enterGroup: EnterNode<T>[], updateGroup: Element[], exitGroup: Element[], data: T[], key: Key<T>) {
+  const elementsByKeyValue = {};
+  const elementsLength = elements.length
+  const dataLength = data.length
+  const keyValues = new Array(elementsLength)
+
+  // Compute the key for each node.
+  // If multiple nodes have the same key, the duplicates are added to exit.
+  for (let i = 0; i < elementsLength; ++i) {
+    let element = elements[i];
+
+    if (element) {
+      let keyValue = KEY_PREFIX + key(element["__data__"], i);
+
+      keyValues[i] = keyValue;
+
+      if (keyValue in elementsByKeyValue) {
+        exitGroup[i] = element;
+      } else {
+        elementsByKeyValue[keyValue] = element;
+      }
+    }
+  }
+
+  // Compute the key for each datum.
+  // If there a node associated with this key, join and add it to update.
+  // If there is not (or the key is a duplicate), add it to enter.
+  for (let i = 0; i < dataLength; ++i) {
+    let keyValue = KEY_PREFIX + key(data[i], i);
+    let element = elementsByKeyValue[keyValue];
+
+    if (element) {
+      updateGroup[i] = element;
+      element["__data__"] = data[i];
+      elementsByKeyValue[keyValue] = null;
+    } else {
+      enterGroup[i] = new EnterNode(parent, data[i]);
+    }
+  }
+
+  // Add any remaining nodes that were not bound to data to exit.
+  for (let i = 0; i < elementsLength; ++i) {
+    const element = elements[i];
+
+    if (element && (elementsByKeyValue[keyValues[i]] === element)) {
+      exitGroup[i] = element;
+    }
+  }
+}
+
+interface Key<T> {
+  (datum: T, index: number): string;
+}
+
+export function generalUpdatePattern<T>(parent: Element, selector: string, data: T[], key: Key<T> | null = null) {
+  const elements = parent.querySelectorAll(selector);
+  const elementsLength = elements.length;
+  const dataLength = data.length;
+  const enterGroup: EnterNode<T>[] = new Array(dataLength);
+  const enteredGroup: Element[] = new Array(dataLength);
+  const updateGroup: Element[] = new Array(dataLength);
+  const exitGroup: Element[] = new Array(elementsLength);
+
+  if (key == null) {
+    bindByIndex(parent, elements, enterGroup, updateGroup, exitGroup, data)
+  }
+  else {
+    bindByKey(parent, elements, enterGroup, updateGroup, exitGroup, data, key)
+  }
 
   for (let i0 = 0, i1 = 0, previous, next; i0 < dataLength; ++i0) {
     if (previous = enterGroup[i0]) {
@@ -40,6 +109,7 @@ export function generalUpdatePattern<T>(parent: Element, selector: string, data:
     enter(cb: (d: T, i: number) => Element) {
       return enterGroup.map((element, index) => {
         enteredGroup[index] = element.appendChild(cb(data[index], index))
+        enteredGroup[index]["__data__"] = data[index]
       }), that
     },
 
@@ -57,8 +127,8 @@ export function generalUpdatePattern<T>(parent: Element, selector: string, data:
       return that;
     },
 
-    exit(cb: (element: Element, index: number) => void) {
-      return exitGroup.map((element, index) => cb(element, index)), that
+    exit(cb: (element: Element, datum: T, index: number) => void) {
+      return exitGroup.map((element, index) => cb(element, element["__data__"], index)), that
     }
   }
 
