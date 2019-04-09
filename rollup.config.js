8cb57968ade2aecae7ccda5382a551cf6398720f
Andrew <fedyk72@gmail.com>
2019-04-09 07:11:42 +0200

    Update

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from "rollup-plugin-typescript2";
+import compiler from "@ampproject/rollup-plugin-closure-compiler";
+
+
+export default {
+  input: "./src/index.ts",
+  
+  output: {
+    file: "./dist/index.iife.js",
+    format: "iife",
+    name: "library"
+  },
+
+	plugins: [
+    typescript({
+      objectHashIgnoreUnknownHack: true
+    }),
+    compiler(),
+	]
+}
